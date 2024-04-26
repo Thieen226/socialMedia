df495cc4ea61d6f9ee97e1723fedc43f50c0c236
Thieen226 <t.ho1@my.metroed.net>
2024-04-26 08:19:43 -0700

    add

---
+++ socialMediaApp/socialMediaApp/socialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  socialMediaAppApp.swift
+//  socialMediaApp
+//
+//  Created by StudentAM on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct socialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
